6e2d676cd12cfbd300a206030b237198d5578e89
fawnsource <hariz.akmal2710@students.amikom.ac.id>
2024-03-05 10:26:37 +0700

    Pelengkap terakir

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 7;
 		
+		Console.Writeline("Hasil penambahan : {0} + {1} = {2} ",a ,b , Penambahan(a,b));
+		ConsoleConsole.Writeline("Hasil penambahan : {0} + {1} = {2} ",a ,b , Penambahan(a,b));
+		
+		Console.Writeline("\n Tekan sembarang untuk keluar");
+		Console.Readkey();
 	}
 	
 	static int Penambahan (int a, int b)
